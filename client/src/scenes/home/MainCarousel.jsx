47e1373bd63d6ab363f06398d69be298b190872c
stepweaver <weaverst@mail.gvsu.edu>
2024-08-09 10:36:59 -0400

    Remove unused import

--- client/src/scenes/home/MainCarousel.jsx
+++ client/src/scenes/home/MainCarousel.jsx
@@ -1,10 +1,8 @@
-// TODO: Replace with a hero image for now
 import { Box, Typography, IconButton, useMediaQuery } from '@mui/material';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import { shades } from '../../theme';
 import heroImage from '../../assets/Brewed Awakening.jpg';
 
 // Imports all images from the assets folder
